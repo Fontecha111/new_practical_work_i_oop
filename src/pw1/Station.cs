6f1ffacb16928141bfe03c8e59d22590c3f3808b
Fontecha111 <9404133@alumnos.ufv.es>
2025-06-22 18:15:04 +0200

    Created Station.cs

---
+++ src/pw1/Station.cs
@@ -0,0 +1,86 @@
+using System;
+
+namespace practicalwork
+{
+    public class Station
+    {
+        private List<Train> trains;
+        private List<Platform> platforms;
+
+        public Station(int numberOfPlatforms)
+        {
+            trains = new List<Train>();
+            platforms = new List<Platform>();
+
+            for (int i = 1; i <= numberOfPlatforms; i++)
+            {
+                platforms.Add(new Platform("P" + i));
+            }
+        }
+
+        public void AdvanceTick()
+        {
+            foreach (var train in trains)
+            {
+                if (train.GetStatus() == Status.EnRoute)
+                {
+                    train.DecreaseArrivalTime(15);
+
+                    if (train.GetArrivalTime() == 0)
+                    {
+                        Platform freePlatform = platforms.FirstOrDefault(p => p.GetStatus() == PlatformStatus.Free);
+
+                        if (freePlatform != null)
+                        {
+                            freePlatform.AssignTrain(train);
+                        }
+                        else
+                        {
+                            train.SetStatus(Status.Waiting);
+                        }
+                    }
+                }
+                else if (train.GetStatus() == Status.Waiting)
+                {
+                    Platform freePlatform = platforms.FirstOrDefault(p => p.GetStatus() == PlatformStatus.Free);
+
+                    if (freePlatform != null)
+                    {
+                        freePlatform.AssignTrain(train);
+                    }
+                }
+            }
+
+            foreach (var platform in platforms)
+            {
+                platform.AdvanceTick();
+                platform.FreePlatform();
+            }
+        }
+
+        public void DisplayStatus()
+        {
+            Console.WriteLine("----Train Status----");
+            foreach (var train in trains)
+            {
+                Console.WriteLine(train.ShowInfo());
+            }
+
+            Console.WriteLine("----Platform Status----");
+            foreach (var platform in platforms)
+            {
+                Console.WriteLine(platform.ShowInfo());
+            }
+        }
+
+        public bool AllTrainsDocked()
+        {
+            return trains.All(t => t.GetStatus() == Status.Docked);
+        }   
+
+        public void LoadTrainsFromFile(string filePath)
+        {
+            
+        }
+    }
+}
