ddd2a50f07b33d8a3e1b74fb3606b32757582ac1
Fontecha111 <9404133@alumnos.ufv.es>
2025-06-22 20:00:25 +0200

    Created the different options for the menu

--- src/pw1/Program.cs
+++ src/pw1/Program.cs
@@ -19,7 +19,34 @@ namespace practicalwork
 
                 string input = Console.ReadLine();
 
-                
+                switch (input)
+                {
+                    case "1":
+                        Console.WriteLine("Enter the file name: ");
+                        string fileName = Console.ReadLine();
+                        station.LoadTrainsFromFile(fileName);
+                        break;
+
+
+                    case "2":
+                        Console.WriteLine("Starting simulation...");
+                        while (!station.AllTrainsDocked()) 
+                        {
+                            station.AdvanceTick();
+                            station.DisplayStatus();
+                            Thread.Sleep(2500);
+                        }
+                        Console.WriteLine("All the trains are docked. Simulation is finished");
+                        break;
+
+                    case "3":
+                        exit = true;
+                        break;
+
+                    default:
+                        Console.WriteLine("That is not a valid option to choose. Try again.");
+                        break;
+                }
             }
         }
     }
