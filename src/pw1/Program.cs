e6042472f2d846f771a37d1463f5c2d4dad6b808
Fontecha111 <9404133@alumnos.ufv.es>
2025-06-22 19:17:01 +0200

    Created the menu for the program

--- src/pw1/Program.cs
+++ src/pw1/Program.cs
@@ -1,1 +1,26 @@
-﻿Console.WriteLine("Write your code here for practical work 1!!!");
+﻿using System;
+
+namespace practicalwork
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Station station = new Station(6);
+            bool exit = false;
+
+            while (!exit)
+            {
+                Console.WriteLine("----- UFV Train Station -----");
+                Console.WriteLine("Choose an action:");
+                Console.WriteLine("1. Load trains from an existing file");
+                Console.WriteLine("2. Start simulation");
+                Console.WriteLine("3. Exit");
+
+                string input = Console.ReadLine();
+
+                
+            }
+        }
+    }
+}
